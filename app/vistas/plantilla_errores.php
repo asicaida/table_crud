3358ddbd5fbfbced4a627fdde772a97e898231ef
Dayana <dayana.sofia23@gmail.com>
2014-01-31 16:10:30 +0100

    cambio en plantilla_error

--- app/vistas/plantilla_errores.php
+++ app/vistas/plantilla_errores.php
@@ -31,7 +31,7 @@
 	</head>
 
 	<body style='border: 1px solid; width: 995px; margin-left: auto; margin-right: auto;  padding: 5px;'>
-		<h1>Error en la aplicación: </h1>
+		<h1>Documento no encontrado. </h1>
 		<?php 
 			if (isset($datos['mensaje']))
 			echo "<p>{$datos['mensaje']}</p>";
