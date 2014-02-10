f72d80916077fcf88956d718ac631d64d637d579
Dayana <dayana.sofia23@gmail.com>
2014-02-10 01:38:26 +0100

    FINAL

--- app/controladores/viajes.php
+++ app/controladores/viajes.php
@@ -9,152 +9,156 @@ class viajes extends \core\Controlador {
 	 * Presenta una <table> con las filas de la tabla con igual nombre que la clase.
 	 * @param array $datos
 	 */
-    public function index(array $datos=array()){
-                   $clausulas['ordey_by']='nombre';
-                   $datos["filas"]= \modelos\Datos_SQL::table("viajes")->select( $clausulas );
-                   $datos['view_content']= \core\Vista::generar(__FUNCTION__, $datos);
-                   $http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
-                   \core\HTTP_Respuesta::enviar($http_body);
-    }
-    
-    
-    
-    
-	
+	public function index(array $datos=array()) {
 		
-	
-	public function form_insertar(array $datos=array()) {
-		
-		$clausulas['order_by'] = " nombre ";
-		$datos['viajes'] = \modelos\Datos_SQL::table("viajes")->select($clausulas);
+		$clausulas['order_by'] = 'nombre';
+		//$datos["filas"] = \modelos\viajes::select($clausulas, "viajes"); // Recupera todas las filas ordenadas
+		$datos["filas"] = \modelos\Modelo_SQL::table("viajes")->select($clausulas); // Recupera todas las filas ordenadas
 		
 		$datos['view_content'] = \core\Vista::generar(__FUNCTION__, $datos);
 		$http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
 		\core\HTTP_Respuesta::enviar($http_body);
 		
 	}
+	
+	
+	public function form_insertar(array $datos=array()) {
+		
+		$datos["form_name"] = __FUNCTION__;
+		$datos['view_content'] = \core\Vista::generar(__FUNCTION__, $datos);
+		$http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
+		\core\HTTP_Respuesta::enviar($http_body);
+		
+	}
 
-	public function validar_form_insertar(array $datos=array())
-	{	
-		$validaciones=array(
-			 "nombre" =>"errores_requerido && errores_texto"
-                        ,"apellidos"=>"errores_requerido  && errores_texto"
-                        ,"destino"=>"errores_requerido  && errores_texto"
-			, "precio" => "errores_precio && errores_requerido"
-			
+	public function validar_form_insertar(array $datos=array()) {
+		
+		
+		
+		$validaciones = array(
+			 "nombre" =>"errores_requerido && errores_texto "
+                         , "apellidos" =>"errores_requerido && errores_texto "
+                     ,"destino" =>"errores_requerido && errores_texto "
+			,"precio" => "errores_precio && errores_requerido"
 		);
 		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos))
             $datos["errores"]["errores_validacion"]="Corrige los errores.";
 		else {
-			$datos['values']['precio'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['precio']);
-			if ( ! $validacion = \modelos\Datos_SQL::table("viajes")->insert($datos["values"])) // Devuelve true o false
+                    $datos['values']['precio'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['precio']);
+                    
+                        
+			
+			if ( ! $validacion = \modelos\Modelo_SQL::insert($datos["values"], 'viajes')) // Devuelve true o false
 				$datos["errores"]["errores_validacion"]="No se han podido grabar los datos en la bd.";
 		}
 		if ( ! $validacion) //Devolvemos el formulario para que lo intente corregir de nuevo
-			$this->cargar_controlador('viajes', 'form_insertar',$datos);
+			\core\Distribuidor::cargar_controlador('viajes', 'form_insertar', $datos);
 		else
 		{
 			// Se ha grabado la modificación. Devolvemos el control al la situacion anterior a la petición del form_modificar
-			$datos = array("alerta" => "Se han grabado correctamente los detalles");
+			//$datos = array("alerta" => "Se han grabado correctamente los detalles");
 			// Definir el controlador que responderá después de la inserción
-			$this->cargar_controlador('viajes', 'index',$datos);		
+			//\core\Distribuidor::cargar_controlador('viajes', 'index', $datos);
+			$_SESSION["alerta"] = "Se han grabado correctamente los detalles";
+			//header("Location: ".\core\URL::generar("viajes/index"));
+			\core\HTTP_Respuesta::set_header_line("location", \core\URL::generar("viajes/index"));
+			\core\HTTP_Respuesta::enviar();
 		}
 	}
 
 	
 	
-	public function form_modificar(array $datos=array()) {
-		
-		if ( ! count($datos)) { // Si no es un reenvío desde una validación fallida
-			$validaciones=array(
-				"id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/viajes/id"
-			);
-			if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
-				$datos['mensaje'] = 'Datos erróneos para identificar el artículo a modificar';
-				$datos['url_continuar'] = \core\URL::generar("viajes");
-				
-				$this->cargar_controlador('mensajes', 'mensaje', $datos);
-				return;
-			}
-			else {
-				$clausulas['where'] = " id = {$datos['values']['id']} ";
-				if ( ! $filas = \modelos\Datos_SQL::table("viajes")->select($clausulas)) {
-					$datos['mensaje'] = 'Error al recuperar la fila de la base de datos';
-					$this->cargar_controlador('mensajes', 'mensaje', $datos);
-					return;
-				}
-				else {
-					$datos['values'] = $filas[0];
+	public function form_modificar(array $datos = array()) {
+
+        if (!count($datos)) { // Si no es un reenvío desde una validación fallida
+            $validaciones = array(
+                "id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/viajes/id"
+            );
+            if (!$validacion = !\core\Validaciones::errores_validacion_request($validaciones, $datos)) {
+                $datos['mensaje'] = 'Datos erróneos para identificar el artículo a modificar';
+                $datos['url_continuar'] = \core\URL::generar("viajes");
+
+                $this->cargar_controlador('mensajes', 'mensaje', $datos);
+                return;
+            } 
+            else {
+                $clausulas['where'] = " id = {$datos['values']['id']} ";
+                if (!$filas = \modelos\Datos_SQL::table("viajes")->select($clausulas)) {
+                    $datos['mensaje'] = 'Error al recuperar la fila de la base de datos';
+                    $this->cargar_controlador('mensajes', 'mensaje', $datos);
+                    return;
+                } 
+                else {
+                    $datos['values'] = $filas[0];
 					$datos['values']['precio'] = \core\Conversiones::decimal_punto_a_coma_y_miles($datos['values']['precio']);
-					
-					$clausulas = array('order_by' => " nombre ");
-					$datos['viajes'] = \modelos\Datos_SQL::table("viajes")->select( $clausulas);
-				}
-			}
-		}
-		
-		$datos['view_content'] = \core\Vista::generar(__FUNCTION__, $datos);
-		$http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
-		\core\HTTP_Respuesta::enviar($http_body);
-	}
 
-	public function validar_form_modificar(array $datos=array()) {	
-		
-		$validaciones=array(
+                    $clausulas = array('order_by' => " nombre ");
+                    $datos['viajes'] = \modelos\Datos_SQL::table("viajes")->select($clausulas);
+                }
+            }
+        }
+
+        $datos['view_content'] = \core\Vista::generar(__FUNCTION__, $datos);
+        $http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
+        \core\HTTP_Respuesta::enviar($http_body);
+    }
+
+    public function validar_form_modificar(array $datos = array()) {
+
+        $validaciones=array(
 			 "id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/viajes/id"
-			, "nombre" =>"errores_requerido && errores_texto && errores_unicidad_modificar:id,nombre/viajes/nombre,id"
-                        , "apellidos" =>"errores_requerido && errores_texto && errores_unicidad_modificar:id,nombre/viajes/nombre,id"
-			, "precio" => "errores_precio"
+			, "nombre" =>"errores_requerido && errores_texto "
+                        , "apellidos" =>"errores_requerido && errores_texto "
+			, "precio" => "errores_precio &&errores_requerido "
 			, "destino" => "errores_requerido"
 		);
-		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
-			//print_r($datos);
+        if (!$validacion = !\core\Validaciones::errores_validacion_request($validaciones, $datos)) {
+            print_r($datos);
             $datos["errores"]["errores_validacion"] = "Corrige los errores.";
-		}
-		else {
-			$datos['values']['precio'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['precio']);
-			
-                       
-			if ( ! $validacion = \modelos\Datos_SQL::table("viajes")->update($datos["values"])) // Devuelve true o false
-				$datos["errores"]["errores_validacion"]="No se han podido grabar los datos en la bd.";
-		}
-		if ( ! $validacion) //Devolvemos el formulario para que lo intente corregir de nuevo
-			$this->cargar_controlador('viajes', 'form_modificar',$datos);
-		else
-		{
-			$datos = array("alerta" => "Se han modificado correctamente.");
-			// Definir el controlador que responderá después de la inserción
-			$this->cargar_controlador('viajes', 'index',$datos);		
-		}
-	}
+        } 
+        else {
+            $datos['values']['precio'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['precio']);
+            if (!$validacion = \modelos\Datos_SQL::table("viajes")->update($datos["values"])) // Devuelve true o false
+                $datos["errores"]["errores_validacion"] = "No se han podido grabar los datos en la bd.";
+        }
+        if (!$validacion) //Devolvemos el formulario para que lo intente corregir de nuevo
+            $this->cargar_controlador('viajes', 'form_modificar', $datos);
+        else {
+            $datos = array("alerta" => "Se han modificado correctamente.");
+            // Definir el controlador que responderá después de la inserción
+           
+            \core\HTTP_Respuesta::set_header_line("location", \core\URL::generar("viajes/index"));
+            $this->cargar_controlador('viajes', 'index', $datos);
+        }
+    }
+
 
 	
 	
 	public function form_borrar(array $datos=array()) {
 		
+		$datos["form_name"] = __FUNCTION__;
 		$validaciones=array(
 			"id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/viajes/id"
 		);
 		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
 			$datos['mensaje'] = 'Datos erróneos para identificar el artículo a borrar';
 			$datos['url_continuar'] = \core\URL::http('?menu=viajes');
-			$this->cargar_controlador('mensajes', 'mensaje', $datos);
+			\core\Distribuidor::cargar_controlador('mensajes', 'mensaje', $datos);
 			return;
 		}
 		else {
 			$clausulas['where'] = " id = {$datos['values']['id']} ";
-			if ( ! $filas = \modelos\Datos_SQL::table("viajes")->select( $clausulas)) {
+			if ( ! $filas = \modelos\Datos_SQL::select( $clausulas, 'viajes')) {
 				$datos['mensaje'] = 'Error al recuperar la fila de la base de datos';
-				$this->cargar_controlador('mensajes', 'mensaje', $datos);
+				\core\Distribuidor::cargar_controlador('mensajes', 'mensaje', $datos);
 				return;
 			}
 			else {
 				$datos['values'] = $filas[0];
-				$datos['values']['precio'] = \core\Conversiones::decimal_punto_a_coma($datos['values']['precio']);
-				$clausulas = array('order_by' => " nombre ");
-				$datos['viajes'] = \modelos\Datos_SQL::select( $clausulas, 'viajes');
 			}
 		}
+		
 		$datos['view_content'] = \core\Vista::generar(__FUNCTION__, $datos);
 		$http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
 		\core\HTTP_Respuesta::enviar($http_body);
@@ -165,19 +169,15 @@ class viajes extends \core\Controlador {
 	
 	
 	
-	
-	
-	
-	
-	
 	public function validar_form_borrar(array $datos=array()) {	
+		
 		$validaciones=array(
 			 "id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/viajes/id"
 		);
 		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
 			$datos['mensaje'] = 'Datos erróneos para identificar el artículo a borrar';
 			$datos['url_continuar'] = \core\URL::http('?menu=viajes');
-			$this->cargar_controlador('mensajes', 'mensaje', $datos);
+			\core\Distribuidor::cargar_controlador('mensajes', 'mensaje', $datos);
 			return;
 		}
 		else
@@ -185,15 +185,16 @@ class viajes extends \core\Controlador {
 			if ( ! $validacion = \modelos\Datos_SQL::delete($datos["values"], 'viajes')) {// Devuelve true o false
 				$datos['mensaje'] = 'Error al borrar en la bd';
 				$datos['url_continuar'] = \core\URL::http('?menu=viajes');
-				$this->cargar_controlador('mensajes', 'mensaje', $datos);
+				\core\Distribuidor::cargar_controlador('mensajes', 'mensaje', $datos);
 				return;
 			}
 			else
 			{
 			$datos = array("alerta" => "Se borrado correctamente.");
-			$this->cargar_controlador('viajes', 'index',$datos);		
+			\core\Distribuidor::cargar_controlador('viajes', 'index', $datos);		
 			}
 		}
+		
 	}
 	
 	
@@ -204,9 +205,9 @@ class viajes extends \core\Controlador {
 		);
 		\core\Validaciones::errores_validacion_request($validaciones, $datos);
 		if (isset($datos['values']['nombre'])) 
-			$clausulas['where'] = " nombre like '%{$datos['values']['nombre']}%'";
-		$clausulas['order_by'] = 'nombre';
-		$datos['filas'] = \modelos\Datos_SQL::select( $clausulas , 'viajes');		
+			$select['where'] = " nombre like '%{$datos['values']['nombre']}%'";
+		$select['order_by'] = 'nombre';
+		$datos['filas'] = \modelos\Datos_SQL::select( $select, 'viajes');		
 		
 		$datos['html_para_pdf'] = \core\Vista::generar(__FUNCTION__, $datos);
 		
@@ -224,13 +225,15 @@ class viajes extends \core\Controlador {
 		$dompdf->stream("sample.pdf", array("Attachment" => 0));
 		
 		// \core\HTTP_Respuesta::set_mime_type('application/pdf');
+		// $http_body = \core\Vista_Plantilla::generar('plantilla_principal', $datos);
 		// \core\HTTP_Respuesta::enviar($datos, 'plantilla_pdf');
 		
 	}
 	
 
 	/**
-	 * Genera una respuesta json con un array que contiene objetos, siendo cada objeto una fila.
+	 * Genera una respuesta json.
+	 * 
 	 * @param array $datos
 	 */
 	public function listado_js(array $datos=array()) {
@@ -240,9 +243,9 @@ class viajes extends \core\Controlador {
 		);
 		\core\Validaciones::errores_validacion_request($validaciones, $datos);
 		if (isset($datos['values']['nombre'])) 
-			$clausulas['where'] = " nombre like '%{$datos['values']['nombre']}%'";
-		$clausulas['order_by'] = 'nombre';
-		$datos['filas'] = \modelos\Datos_SQL::select($clausulas, 'viajes');
+			$select['where'] = " nombre like '%{$datos['values']['nombre']}%'";
+		$select['order_by'] = 'nombre';
+		$datos['filas'] = \modelos\Datos_SQL::select($select, 'viajes');
 				
 		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
 		
@@ -263,9 +266,9 @@ class viajes extends \core\Controlador {
 		);
 		\core\Validaciones::errores_validacion_request($validaciones, $datos);
 		if (isset($datos['values']['nombre'])) 
-			$clausulas['where'] = " nombre like '%{$datos['values']['nombre']}%'";
-		$clausulas['order_by'] = 'nombre';
-		$datos['filas'] = \modelos\Datos_SQL::select( $clausulas, 'viajes');
+			$select['where'] = " nombre like '%{$datos['values']['nombre']}%'";
+		$select['order_by'] = 'nombre';
+		$datos['filas'] = \modelos\Datos_SQL::select( $select, 'viajes');
 				
 		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
 		
@@ -288,14 +291,15 @@ class viajes extends \core\Controlador {
 		);
 		\core\Validaciones::errores_validacion_request($validaciones, $datos);
 		if (isset($_datos['values']['nombre'])) 
-			$clausulas['where'] = " nombre like '%{$_datos['values']['nombre']}%'";
-		$clausulas['order_by'] = 'nombre';
-		$datos['filas'] = \modelos\Datos_SQL::select( $clausulas, 'viajes');
+			$select['where'] = " nombre like '%{$_datos['values']['nombre']}%'";
+		$select['order_by'] = 'nombre';
+		$datos['filas'] = \modelos\Datos_SQL::select( $select, 'viajes');
 				
 		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
 		
 		\core\HTTP_Respuesta::set_mime_type('text/xml');
-		\core\HTTP_Respuesta::enviar('plantilla_xml', $datos);
+		$http_body = \core\Vista_Plantilla::generar('plantilla_xml', $datos);
+		\core\HTTP_Respuesta::enviar($http_body);
 		
 	}
 	
@@ -313,9 +317,9 @@ class viajes extends \core\Controlador {
 		);
 		\core\Validaciones::errores_validacion_request($validaciones, $datos);
 		if (isset($_datos['values']['nombre'])) 
-			$clausulas['where'] = " nombre like '%{$_datos['values']['nombre']}%'";
-		$clausulas['order_by'] = 'nombre';
-		$datos['filas'] = \modelos\Datos_SQL::select($clausulas, 'viajes');
+			$select['where'] = " nombre like '%{$_datos['values']['nombre']}%'";
+		$select['order_by'] = 'nombre';
+		$datos['filas'] = \modelos\Datos_SQL::select( $select, 'viajes');
 				
 		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
 		
--- app/vistas/viajes/form_and_inputs.php
+++ app/vistas/viajes/form_and_inputs.php
@@ -7,7 +7,7 @@
 	<?php echo \core\HTML_Tag::span_error('nombre', $datos); ?>
 	<br />
         Apellidos:<input id="apellidos" name="apellidos" type='text' size='100' maxlength='100' value='<?php echo \core\Array_Datos::values ( 'apellidos', $datos);?>' />
-        <?php echo \core\HTML_Tag::span_error('nombre', $datos);?>
+        <?php echo \core\HTML_Tag::span_error('apellidos', $datos);?>
         <br />
         Destino:<input id='destino' name='destino' type='text' size='100'  maxlength='100' value='<?php echo \core\Array_Datos::values('destino', $datos);?>'/>
 	<?php echo \core\HTML_Tag::span_error('destino', $datos); ?>
@@ -15,6 +15,7 @@
         Precio:<input id='precio'  name='precio' type='text' size='15' maxlength="15" value='<?php echo \core\Array_Datos::values('precio', $datos);?>' />
         <br />
         <?php echo \core\HTML_Tag::span_error('precio', $datos); ?>
+        <br />
         Fecha de compra :<input id='fecha' name='fecha' type="text" size="50" maxlength="50" value="<?php echo \core\Array_Datos::values('fecha_de_compra', $datos);?>" />
 	<br />
          <?php echo \core\HTML_Tag::span_error('errores_validacion', $datos); ?>
